0757efd7b2fb2256246d97ddd720df7eddc25c99
Samir <imnotajedi@icloud.com>
2022-12-31 00:53:38 +0700

    Initial Commit

---
+++ MerryChristmasApp/MerryChristmasAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MerryChristmasAppApp.swift
+//  MerryChristmasApp
+//
+//  Created by Самир Кафаров on 31.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct MerryChristmasAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
